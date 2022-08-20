3f0b15db5945311bba2f3d0014a5914ee08eb788
Lesko <polensek.matevz@gmail.com>
2022-08-20 19:55:04 +0200

    Configuring pytest workflow.

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+class TestConfig:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'test_app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 # DB_IMPORT_FILE_PATH = os.path.join(basedir, r"./upload/report.txt")
 #
 # ALLOWED_EXTENSIONS = {"txt", }
